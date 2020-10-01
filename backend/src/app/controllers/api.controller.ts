caa8a72a1567c6bfdd621f6f30fcc2e1082ffa9b
Samuel Schwab <sam2schwab@gmail.com>
2020-09-30 23:54:12 -0400

    cleanup du code backend

--- backend/src/app/controllers/api.controller.ts
+++ backend/src/app/controllers/api.controller.ts
@@ -1,15 +1,8 @@
 import { Context, Get, HttpResponse, HttpResponseOK } from '@foal/core';
-import { CentreServiceScolaire } from '../../../../common/src/schemas';
 
 export class ApiController {
     @Get('/')
     index(ctx: Context): HttpResponse {
-        return new HttpResponseOK('Helloooooooo world!');
-    }
-
-    @Get('/centres-service-scolaire')
-    css(ctx: Context): HttpResponse {
-        const data: CentreServiceScolaire[] = [{ id: 2, name: 'test' }];
-        return new HttpResponseOK(data);
+        return new HttpResponseOK('Hello world!');
     }
 }
